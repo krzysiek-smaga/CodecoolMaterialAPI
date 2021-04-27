d899c3265725e06533e74500e3277aa838308e56
Krzysiek <krzysiek.at@gmail.com>
2021-04-27 14:25:17 +0200

    Add empty profiles and DTOs files for each model

---
+++ CodecoolMaterialAPI/Profiles/ReviewProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodecoolMaterialAPI.DAL.Models;
+
+namespace CodecoolMaterialAPI.Profiles
+{
+    public class ReviewProfile : Profile
+    {
+        public ReviewProfile()
+        {
+
+        }
+    }
+}
